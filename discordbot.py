ea97dae1384e0754e4fc1a75dac72860a9e7f7eb
maobushi <85619868+maobushi@users.noreply.github.com>
2023-11-28 23:51:51 +0900

    add main.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,25 @@
 import discord
-import traceback
-from discord.ext import commands
-from os import getenv
+import openai
+import os
 
-intents = discord.Intents.default()
-intents.message_content = True
+client = discord.Client()
+openai_api_key = os.environ['GPT_API_KEY']  # OpenAI APIキーを設定
+discord_bot_token = os.environ['DISCORD_BOT_TOKEN'] # Discord BOTトークンを設定
+target_emoji = "👍"  # 反応する絵文字を設定
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.emoji == target_emoji:
+        message = reaction.message
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "You are a helpful assistant."},
+                      {"role": "user", "content": message.content}]
+        )
+        await message.channel.send(response.choices[0].message['content'])
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
-
+client.run(discord_bot_token)
